b7e0f5a780ca6608c4956d9753554467fe4c56e3
Сергей <1@php-cat.com>
2025-07-26 02:50:18 +0500

    старт

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.php",
+    "./widgets/**/*.php",
+    "./web/**/*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ views/site/index.php
@@ -0,0 +1,83 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">Поздравляю!</h1>
+
+        <p class="lead">Меня зовут Сергей Бакланов, я делаю всякие интернет сервисы, штуки и сайты.</p>
+        <p class="lead">Мой сайт <a href="https://php-cat.com"
+                                    class="text-blue-500 underline"
+                                    target="_blank" >php-cat.com</a></p>
+
+<!--        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>-->
+    </div>
+
+<div class=" container">
+    <div class="flex flex-row w-[600px] mx-auto shadow-lg rounded p-3 border-2 border-orange-300">
+        <div class="w-[350px]">
+            <p>Сайт создан для отработки навыков работы с YII сайтами</p>
+            <p>работа с навигацией страницами</p>
+            <p>работа с бд</p>
+            <p>показ формочки и её обработка</p>
+            <p>интеграция АПИ других служб</p>
+        </div>
+        <div class="w-[250px]">
+            <img src="/img/kosmo2.jpeg" alt=""/>
+        </div>
+    </div>
+</div>
+    <div class="hidden body-content">
+
+        <div class="row">
+            <div class="col-lg-4 mb-3">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation
+                        &raquo;</a></p>
+            </div>
+            <div class="col-lg-4 mb-3">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a>
+                </p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions
+                        &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
